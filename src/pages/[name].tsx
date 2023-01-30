ec8d183dd38ecb0e8f2a0abece7e16a239175753
takopon <doluche.avalanche@gmail.com>
2023-01-31 00:45:21 +0900

    feedを呼び出すのではなく、tweet.tsxを呼び出すようにすれば

--- src/pages/[name].tsx
+++ src/pages/[name].tsx
@@ -1,12 +1,18 @@
 import { useRouter } from "next/router";
 import React from "react";
 import Feed from "../components/Feed";
+import Head from "next/head";
 
 export default function UserPage() {
   const router = useRouter();
   const name = router.query.name as string;
+  const id = router.query.id as string;
+
   return (
-    <div>
+    <div className="scrollbar-hide col-span-7 min-h-screen overflow-scroll border-x lg:col-span-5">
+      <Head>
+        <title>{router.query.id}</title>
+      </Head>
       <Feed where={{ author: { name } }} />
     </div>
   );
